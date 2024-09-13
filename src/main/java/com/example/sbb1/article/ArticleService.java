319776cc66d71ecda8747cfce0257466a51afe97
yujihun1 <sk0253642@gmail.com>
2024-09-13 09:51:05 +0900

    article clean

---
+++ src/main/java/com/example/sbb1/article/ArticleService.java
@@ -0,0 +1,40 @@
+package com.example.sbb1.article;
+
+import com.example.sbb1.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class ArticleService {
+    private  final ArticleRepository articleRepository;
+
+    public List<Article> getList(){
+        return this.articleRepository.findAll();
+    }
+
+    public Article getArticle(Integer id){
+        Optional<Article> article =this.articleRepository.findById(id);
+        if(article.isPresent()){
+            return article.get();
+        }else{
+            throw new DataNotFoundException("question not found");
+        }
+
+
+
+    }
+    public void create(String title,String content){
+        Article article =new Article();
+        article.setTitle(title);
+        article.setContent(content);
+        article.setCreateDate(LocalDateTime.now());
+        this.articleRepository.save(article);
+
+
+    }
+}
